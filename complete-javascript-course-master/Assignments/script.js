2d537a22ef321b188995b73721fa4de0b6b54d1f
SweedalMasih <sweedal.v.masih@gmail.com>
2023-07-02 22:08:26 +0530

    The switch Statement

--- complete-javascript-course-master/Assignments/script.js
+++ complete-javascript-course-master/Assignments/script.js
@@ -43,3 +43,25 @@ if (language === "English" && population < 50 && !isIsland) {
 } else {
   console.log(`${country} does not meet your criteria :(`);
 }
+
+// The switch Statement
+switch (language) {
+  case "Chinese":
+  case "Mandarin":
+    console.log(`MOST number of native speakers!`);
+    break;
+  case "Spanish":
+    console.log(`2nd place in number of native speakers`);
+    break;
+  case "English":
+    console.log(`3rd place`);
+    break;
+  case "Hindi":
+    console.log(`Number 4`);
+    break;
+  case "Arabic":
+    console.log(`5th most spoken language`);
+    break;
+  default:
+    console.log(`Great language too :D`)
+}
