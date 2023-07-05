98c8fa23b5210a2fa116c064b24eb2a33e87c512
SweedalMasih <sweedal.v.masih@gmail.com>
2023-07-05 22:02:42 +0530

    Coding Challenge #2

--- complete-javascript-course-master/Assignments/script.js
+++ complete-javascript-course-master/Assignments/script.js
@@ -90,4 +90,10 @@ const johnBMI = weightOfJohn / (heightOfJohn ** 2);
 
 const markHigherBMI = markBMI > johnBMI;
 
-console.log(markBMI, johnBMI, markHigherBMI);
+// Coding Challenge #2
+
+if (markHigherBMI) {
+  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`);
+} else {
+  console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+}
