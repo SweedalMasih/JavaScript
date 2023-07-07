b8a625067587a4ea2444f378a37d893923756846
SweedalMasih <sweedal.v.masih@gmail.com>
2023-07-07 23:13:32 +0530

    Coding Challenge #4

--- complete-javascript-course-master/Assignments/script.js
+++ complete-javascript-course-master/Assignments/script.js
@@ -123,3 +123,18 @@ if (avgDolpins > avgKoalas && avgDolpins >= 100) {
 } else {
   console.log("No team wins the trophy");
 }
+
+// Coding Challenge #4
+
+// Test data 1
+const bill = 275;
+
+// Test data 2
+// const bill = 40;
+
+// Test data 3
+// const bill = 430;
+
+const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
