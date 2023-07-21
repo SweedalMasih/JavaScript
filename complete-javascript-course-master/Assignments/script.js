c615944143442217e0d97e94a1e53bae6ab430b1
SweedalMasih <sweedal.v.masih@gmail.com>
2023-07-21 23:36:41 +0530

    Looping Backwards and Loops in Loops

--- complete-javascript-course-master/Assignments/script.js
+++ complete-javascript-course-master/Assignments/script.js
@@ -273,3 +273,17 @@ for (let i = 0; i < populations.length; i++) {
   percentages2[i] = percentageOfWorld1(populations[i]);
 }
 console.log(percentages2);
+
+// Looping Backwards and Loops in Loops
+
+const listOfNeighbours = [
+  ["Canada", "Mexico"],
+  ["Spain"],
+  ["Norway", "Sweden", "Russia"],
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+  for (let j = 0; j < listOfNeighbours[i].length; j++) {
+    console.log(`Neighbour: ${listOfNeighbours[i][j]}`);
+  }
+}
